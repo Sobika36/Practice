b8c0bc2027107a8f1abe6450f735a7de1353ad05
Sobika M <msobika3@gmail.com>
2025-06-29 12:53:38 +0530

    Time: 1 ms (92.65%), Space: 42.3 MB (64.39%) - LeetHub

--- 0171-excel-sheet-column-number/0171-excel-sheet-column-number.java
+++ 0171-excel-sheet-column-number/0171-excel-sheet-column-number.java
@@ -3,9 +3,10 @@ class Solution {
         int sum=0;
         int val=0; 
         for(char c:columnTitle.toCharArray()){
-            sum*=26;
-            val=1+(c-'A');
-            sum+=val;
+            // sum*=26;
+            // val=1+(c-'A');
+            // sum+=val;
+            sum=sum*26+(c-'A'+1);
         }
         return sum;
     }
