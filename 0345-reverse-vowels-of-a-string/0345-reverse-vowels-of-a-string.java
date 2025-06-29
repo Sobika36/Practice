6267fcb62f05a4ffb41920be6ba3dbd9138c841f
Sobika M <msobika3@gmail.com>
2025-06-29 12:00:12 +0530

    Time: 4 ms (49.9%), Space: 45.7 MB (14.19%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
@@ -1,25 +1,44 @@
 class Solution {
     public String reverseVowels(String s) {
-        StringBuilder sb=new StringBuilder(s);
-        int start=0;
-        int end=s.length()-1;
-        while(start<end){
-            char l=sb.charAt(start);
-            char r=sb.charAt(end);
-            if(l!='a' && l!='e' && l!='i' && l!='o' && l!='u' && l!='A' && l!='E' && l!='I' && l!='O' && l!='U'){
-                start++;
+        // StringBuilder sb=new StringBuilder(s);
+        // int start=0;
+        // int end=s.length()-1;
+        // while(start<end){
+        //     char l=sb.charAt(start);
+        //     char r=sb.charAt(end);
+        //     if(l!='a' && l!='e' && l!='i' && l!='o' && l!='u' && l!='A' && l!='E' && l!='I' && l!='O' && l!='U'){
+        //         start++;
+        //     }
+        //     else if(r!='a' && r!='e' && r!='i' && r!='o' && r!='u' && r!='A' && r!='E' && r!='I' && r!='O' && r!='U'){
+        //         end--;
+        //     }
+        //     else{
+        //         char temp=sb.charAt(start);
+        //         sb.setCharAt(start,sb.charAt(end));
+        //         sb.setCharAt(end,temp);
+        //         start++;
+        //         end--;
+        //     }
+        // }
+        // return sb.toString();
+
+        char []letters=s.toCharArray();
+        int i=0;
+        int j=letters.length-1;
+        String ss="aeiouAEIOU";
+        while(i<j){
+            while((!ss.contains(String.valueOf(letters[i]))) && i<j){
+                i++;
             }
-            else if(r!='a' && r!='e' && r!='i' && r!='o' && r!='u' && r!='A' && r!='E' && r!='I' && r!='O' && r!='U'){
-                end--;
+            while((!ss.contains(String.valueOf(letters[j]))) && i<j){
+                j--;
             }
-            else{
-                char temp=sb.charAt(start);
-                sb.setCharAt(start,sb.charAt(end));
-                sb.setCharAt(end,temp);
-                start++;
-                end--;
-            }
+            char temp=letters[i];
+            letters[i]=letters[j];
+            letters[j]=temp;
+            i++;
+            j--;
         }
-        return sb.toString();
+        return new String(letters);
     }
 }
