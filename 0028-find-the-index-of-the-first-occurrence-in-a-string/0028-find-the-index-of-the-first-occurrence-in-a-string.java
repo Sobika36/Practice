95cbe10055765dda1d05a5a371e07c0e76cfa65b
Sobika M <msobika3@gmail.com>
2025-07-02 12:28:18 +0530

    Time: 0 ms (100%), Space: 41.4 MB (89.28%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
@@ -1,24 +1,23 @@
 class Solution {
     public int strStr(String haystack, String needle) {
-        int num=0;
-        // for(int i=0;i<haystack.length();i++){
-        //     for(int j=i;j<needle.length();j++){
-        //         if(haystack.contains(needle)){
-        //             return i;
-        //         }
-        //     }
-            
-        // }
-        // if(haystack.contains(needle)){
-        //     for(int i=0;i<haystack.length();i++){
-        //         for(int j=i;j<haystack.length();j++){
-        //             if(haystack.charAt(i)==haystack.charAt(j)){
-        //                 return i;
-        //             }
-        //         }
-        //     }
-        // }
-        // return -1;
-        return haystack.indexOf(needle);
+        //return haystack.indexOf(needle);
+        int hl=haystack.length();
+        int nl=needle.length();
+        if(needle.isEmpty()){
+            return 0;
+        }
+        for(int i=0;i<=hl-nl;i++){
+            boolean flag=true;
+            for(int j=0;j<nl;j++){
+                if(haystack.charAt(i+j)!=needle.charAt(j)){
+                    flag=false;
+                    break;
+                }
+            }
+            if(flag){
+                return i;
+            }
+        }
+        return -1;
     }
 }
